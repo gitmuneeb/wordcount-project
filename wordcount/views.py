77db0e8efeb34860c27309f6978b63bbbd48718d
Muneeb <muneebbashir0@gmail.com>
2021-09-25 22:59:05 +0530

    Added AboutUs page, count word output will be in sequence

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,12 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+    
 def count(request):
     fulltext = request.GET['fulltext']
 
     wordlist = fulltext.split()
 
-    return render(request, 'count.html',{'fulltext':fulltext,'count':len(wordlist)})
+    worddictionary ={} # words with their count in whole  input passage
+
+    for word in wordlist:
+         if word in worddictionary:
+             #increase the COUNT
+             worddictionary[word] += 1
+         else:
+            #add to thr dictionary
+            worddictionary[word]=1
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html',{'fulltext':fulltext,'count':len(wordlist),'sortedword':sortedwords})
